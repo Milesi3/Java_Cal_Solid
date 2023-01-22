b54b229603f82cf00a81115d35d204c68862fc35
milesi <irina-rxv@yandex.ru>
2023-01-22 13:15:24 +0300

    Создал калькулятор Солид

---
+++ src/model/Number.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Number {
+    double numberReal;
+    double numberImage;
+
+    public double getNumberReal() {
+        return numberReal;
+    }
+
+
+    public double getNumberImage() {
+        return numberImage;
+    }
+
+
+
+}
